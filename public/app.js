6f41d47a8c71332992bdd0657ef0c1023a019564
kolstr <kolja.strauss@gmail.com>
2026-01-17 17:57:01 +0100

    public/app.js aktualisiert

--- public/app.js
+++ public/app.js
@@ -720,7 +720,7 @@ const autoReadState = {
 let markRemainingButton = null;
 
 function setMarkRemainingButtonLabel(button, isWorking) {
-  const icon = '<svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>';
+  const icon = '<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>';
   button.innerHTML = icon;
 }
 
